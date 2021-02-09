2556adb7a20b910d25c9498cd31bfa8d4aac96f9
MurichAJ <aj@murich.info>
2021-02-09 20:39:04 +0200

    wip

--- prerender.config.ts
+++ prerender.config.ts
@@ -1,10 +1,10 @@
 import { PrerenderConfig} from "@stencil/core";
 export const config: PrerenderConfig = {
     
-    hydrateOptions(url: URL = new URL("http://localhost:3333/cats")) {
-        return {
-            removeHtmlComments: false,
-            excludeComponents: ["p"],
-        }
-    }
+    // hydrateOptions(url: URL = new URL("http://localhost:3333/cats")) {
+    //     return {
+    //         removeHtmlComments: false,
+    //         excludeComponents: ["p"],
+    //     }
+    // }
 }
--- src/components/components/example-page/example-page.tsx
+++ src/components/components/example-page/example-page.tsx
@@ -6,10 +6,28 @@ import { Component, h, Host } from "@stencil/core"
 })
 
 export class ExamplePage {
+    peoples;
+    async componentWillLoad() {
+        const ret = await fetch("https://swapi.dev/api/people/");
+        await ret.json().then((data) => {
+            this.peoples = data["results"];    
+        });
+        console.log(this.peoples);
+    }
     render(){
         return(
             <Host>
                 <p>Some data that was fetched.</p>
+                <ol>
+                    {this.peoples.map((unit) => {
+                        return <li>{unit.name}</li>
+                    })}
+                    {/* {this.peoples.map((unit) => {
+                        return <ul>{Object.keys(unit).forEach((key) => {
+                            return <li>{`${key} ${unit[key]}`}</li>
+                        })}</ul>    
+                    })} */}
+                </ol>
             </Host>
         )
     }
